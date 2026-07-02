2f87233336525393fb5bedf62039cebcf61fa469
Eddy Kang <eddykang06@gmail.com>
2026-07-02 16:37:27 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+
+        # Define mapping from closed to open
+        mapping = {
+            "}": "{",
+            "]": "[",
+            ")": "("
+        }
+
+        for c in s:
+            
+            # Push open parentheses to stack
+            if c in mapping.values():
+                stack.append(c)
+            
+            # Check that top of stack matches 
+            else:
+                top = stack[-1] if  stack else None
+                if top == mapping[c]:
+                    stack.pop()
+                else:
+                    return False        
+        return True if not stack else False

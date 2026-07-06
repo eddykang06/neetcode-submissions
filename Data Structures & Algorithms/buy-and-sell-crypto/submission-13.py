b40c63cbfa6270a5d87b3fe338bbda4fde7f6d90
Eddy Kang <eddykang06@gmail.com>
2026-07-05 21:54:38 -0400

    Add: buy-and-sell-crypto - submission-13

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        n = len(prices)
+
+        # O(n) time, O(1) space
+        max_profit = 0
+
+        # Two pointers
+        l = 0
+        r = 1
+
+        while r < n:
+            if prices[l] <= prices[r]:
+                profit = prices[r] - prices[l]
+                if profit > max_profit:
+                    max_profit = profit
+                r += 1
+            else:
+                l = r
+                r = l + 1
+        
+        return max_profit if max_profit > 0 else 0 
